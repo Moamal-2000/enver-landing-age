f7b8cb8052c95418159aa0076b00325fff479a4c
zeyad <tamerziad276@gmail.com>
2023-09-20 18:38:55 +0300

    responsive versiion

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 "use strict"
-
+const slider = document.querySelector(".slider")
+const templatesHolder = document.querySelector(".templates")
+const slideButtons = document.querySelectorAll(".switchers > *")
+let ratio = templatesHolder.offsetWidth / templatesHolder.children.length
+slideButtons.forEach(btn => {
+  btn.onclick = function () {
+    slideButtons.forEach(btn => btn.classList.remove("active"));
+    btn.classList.add("active");
+    if (btn.classList.contains("left")) {
+      slider.scrollBy(ratio,0);
+    }
+    else {
+      slider.scrollBy(-ratio,0);
+    }
+  }
+})
