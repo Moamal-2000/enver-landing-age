193ecbad8768bac7a6bfd5cde77a7c23f1b2f7c9
zeyad <tamerziad276@gmail.com>
2023-09-21 23:38:46 +0300

    last version

--- js/script.js
+++ js/script.js
@@ -4,14 +4,21 @@ const templatesHolder = document.querySelector(".templates")
 const slideButtons = document.querySelectorAll(".switchers > *")
 slideButtons.forEach(btn => {
   btn.onclick = function () {
-    let ratio = templatesHolder.offsetWidth / templatesHolder.children.length
-    slideButtons.forEach(btn => btn.classList.remove("active"));
-    btn.classList.add("active");
+    let ratio = Math.ceil(templatesHolder.offsetWidth / templatesHolder.children.length)
+    
     if (btn.classList.contains("left")) {
-      slider.scrollBy(ratio,0);
+      slider.scrollBy({
+        top:0,
+        left:-ratio,
+        behavior:"smooth",
+      });
     }
     else {
-      slider.scrollBy(-ratio,0);
+      slider.scrollBy({
+        top:0,
+        left:ratio,
+        behavior:"smooth",
+      })
     }
   }
 })
