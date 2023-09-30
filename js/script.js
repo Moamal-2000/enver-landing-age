6b88d38e7cef3c03770a7a17a889b47e749ea9dd
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-09-30 03:56:27 +0300

    cursor effect

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@ const header = document.querySelector('header')
 const slider = document.querySelector(".slider")
 const templatesHolder = document.querySelector(".templates")
 const slideButtons = document.querySelectorAll(".switchers > i")
-
+const liElements = document.querySelectorAll("footer nav ul li")
+const cursorEffectEle = document.querySelector(".cursor-effect")
+const footerLiElements = document.querySelectorAll("footer nav li")
+const allAnchorTags = document.querySelectorAll("a")
+const allButtons = document.querySelectorAll("button")
 
 
 
@@ -15,6 +19,8 @@ const slideButtons = document.querySelectorAll(".switchers > i")
 // Variables
 let isHeaderActive = false
 let lastScrollYValue = 0
+let isMouseEnterNavLinks = true
+const allClickElements = [...footerLiElements, ...allAnchorTags, ...allButtons, ...slideButtons]
 
 
 
@@ -63,6 +69,31 @@ function handleSlidesButtons(btn) {
 }
 
 
+function handleCursorMoveEffect(e) {
+  const positionX = e.clientX;
+  const positionY = e.clientY;
+  const halfWidthCursor = cursorEffectEle.offsetWidth / 2;
+  const halfHeightCursor = cursorEffectEle.offsetHeight / 2;
+
+  cursorEffectEle.style.cssText = `
+  transform: translate(${positionX - halfWidthCursor}px, ${positionY - halfHeightCursor}px)
+  `
+}
+
+
+function handleCursorActive(e, ele) {
+  const positionX = e.offsetX,
+  positionY = e.offsetY,
+  halfWidthLiEle = e.currentTarget.offsetWidth / 2,
+  halfHeightLiEle = e.currentTarget.offsetHeight / 2;
+
+ele.style.cssText = `
+transform: translate(${positionX - halfWidthLiEle}px, ${positionY - halfHeightLiEle}px);
+`
+}
+
+
+
 
 
 // Events
@@ -70,3 +101,35 @@ window.addEventListener('scroll', (e) => handleHeaderOnScroll(e))
 
 
 slideButtons.forEach(btn => btn.addEventListener("click", () => handleSlidesButtons(btn)))
+
+
+window.addEventListener("mousemove", (e) => handleCursorMoveEffect(e))
+
+
+allClickElements.forEach(element => {
+  element.addEventListener("mouseenter", () => {
+    cursorEffectEle.classList.add("active")
+  })
+
+  element.addEventListener("mouseout", () => {
+    cursorEffectEle.classList.remove("active")
+  })
+})
+
+
+liElements.forEach(li => {
+  const anchorTag = li.querySelector("a")
+
+  li.addEventListener("mousemove", (e) => handleCursorActive(e, anchorTag))
+
+  li.addEventListener("mouseout", () => {
+    anchorTag.style.cssText = ""
+    })
+})
+
+
+
+
+
+
+
